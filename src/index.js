073ade1d1bca010db2977960af9ed3b64c2a3135
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2021-12-30 11:45:33 +0100

    webfont dynamic loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebfontLoader from '@dr-kobros/react-webfont-loader';
+
 import App from './App';
 import './scss/main.scss'
 
+const config = {
+    google: {
+        families: ['Open Sans:300,400,600,700', 'Raleway:300,400,700,800'],
+    }
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <WebfontLoader config={config}>
+            <App/>
+        </WebfontLoader>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
