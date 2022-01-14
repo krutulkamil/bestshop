17246cbfe0afbd62baede6193f0febdfa2031169
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2022-01-14 13:11:56 +0100

    conditional nav links

--- frontend/src/components/header/HeaderNav.js
+++ frontend/src/components/header/HeaderNav.js
@@ -2,17 +2,29 @@ import React from 'react';
 import navigationConfig from '../../helpers/navigationConfig.js'
 import HeaderNavElement from "./HeaderNavElement";
 import {Link} from 'react-router-dom';
+import {isAuth} from "../../helpers/functions";
 
 const HeaderNav = ({setIsOpen}) => {
+
+    const logout = () => {
+        localStorage.removeItem('auth');
+    };
 
     return (
         <ul className="header__nav">
             {navigationConfig.map(element => (
                 <HeaderNavElement key={element.id} element={element} setIsOpen={setIsOpen} />
             ))}
-            <li className="nav__element">
-                <Link to="/login" className="nav__link">Login</Link>
-            </li>
+
+            {isAuth() ? (
+                <li className="nav__element">
+                    <Link onClick={logout} to="/login" className="nav__link">Logout</Link>
+                </li>
+            ) : (
+                <li className="nav__element">
+                    <Link to="/login" className="nav__link">Login</Link>
+                </li>
+            )}
         </ul>
     );
 };
